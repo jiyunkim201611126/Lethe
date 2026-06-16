e4d3b4498de33bdba62c07e639acc56cbff8bc6f
jiyunkim201611126 <103517141+jiyunkim201611126@users.noreply.github.com>
2026-06-16 23:24:28 +0900

    5.7 버전업

--- Source/LetheEditor.Target.cs
+++ Source/LetheEditor.Target.cs
@@ -8,8 +8,8 @@ public class LetheEditorTarget : TargetRules
 	public LetheEditorTarget( TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Editor;
-		DefaultBuildSettings = BuildSettingsVersion.V5;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_6;
+		DefaultBuildSettings = BuildSettingsVersion.V6;
+		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_7;
 		ExtraModuleNames.Add("Lethe");
 		RegisterModulesCreatedByRider();
 	}
